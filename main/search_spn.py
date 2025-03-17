3d2ea6cf970063684470b8dd3887be090b39510b
Diana-ya <diana-st.p@yandex.ru>
2025-03-17 17:36:07 +0300

    Update search_spn.py

--- main/search_spn.py
+++ main/search_spn.py
@@ -1,1 +1,7 @@
-
+def find_spn(resp):
+    top = resp["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]
+    x, y = top['boundedBy']['Envelope']['lowerCorner'].split(' '), \
+        top['boundedBy']['Envelope']['upperCorner'].split(' ')
+    width = float(y[0]) - float(x[0])
+    height = float(y[1]) - float(x[1])
+    return str(width), str(height)
